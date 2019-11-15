416720d27d872b9d0bb7da5241c59f5c8127d9d7
Yuset Mercado Vanegas <ymercado@olimpiait.com>
2019-11-14 22:46:05 -0500

    se modifica index.js

--- api/index.js
+++ api/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 
-app.get('/',function (req, res) {
- res.send('saludos desde express');
+app.post('/hola',function (req, res) {
+ res.send('[POST]Saludos desde express');
 });
+
+
+app.get('/hola',function (req, res) {
+    res.send('[GET]Saludos desde express');
+   });
 
 app.listen(3000,() => {
     console.log('el servidor se incio en el puerto 3000');
